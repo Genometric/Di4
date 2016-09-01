3f360a97734e362b09fe87ce50b127795b19beba
VJalili <jalili.vahid@gmail.com>
2016-09-01 14:29:15 +0000

    few updates for benchmarking.

--- Di4/Di4BCLI/Program.cs
+++ Di4/Di4BCLI/Program.cs
@@ -76,6 +76,7 @@ namespace Polimi.DEIB.VahidJalili.DI4.CLI
 
         static void Test2RI()
         {
+            int repeats = 10;
             UserConfigSetup.SetGet();
             Directory.EnumerateFiles(UserConfig.workingDirectory, "*.idx2R").ToList().ForEach(x => File.Delete(x));
             Herald.Initialize(Herald.Destination.Both, UserConfig.logFile);
@@ -90,7 +91,7 @@ namespace Polimi.DEIB.VahidJalili.DI4.CLI
 
             var timer = new Stopwatch();
             timer.Start();
-            orchestrator.Index2ndResolution(new string[] { "2RI", "zt", bin.ToString() });
+            orchestrator.Index2ndResolution(new string[] { "2RI", "uq", bin.ToString() });
             timer.Stop();
             using (var writter = new StreamWriter(UserConfig.workingDirectory + "2riTimeToCreateTest.txt", true))
             {
@@ -98,131 +99,243 @@ namespace Polimi.DEIB.VahidJalili.DI4.CLI
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "1", "2", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t1\t2\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "1", "2", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t1\t2\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "5", "10", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t5\t10\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "5", "10", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t5\t10\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "10", "20", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t10\t20\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "10", "20", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t10\t20\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "50", "60", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t50\t60\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "50", "60", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t50\t60\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "100", "200", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t100\t200\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "80", "90", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t80\t90\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "200", "220", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t200\t220\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "100", "200", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t100\t200\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "400", "500", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t400\t500\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "200", "220", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t200\t220\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "550", "1000", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t550\t1000\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "300", "320", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t300\t320\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "1", "1", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t1\t1\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "400", "500", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t400\t500\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "10", "10", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t10\t10\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "550", "1000", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t550\t1000\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "25", "25", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t25\t25\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "1", "1", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t1\t1\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "50", "50", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t50\t50\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "10", "10", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t10\t10\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "100", "100", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+            for (int r = 0; r < repeats; r++)
             {
-                writter.WriteLine(bin.ToString() + "\t100\t100\t" + timer.Elapsed.TotalSeconds.ToString());
-            }
-
-            timer.Restart();
-            orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "150", "150", "count" });
-            timer.Stop();
-            using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
-            {
-                writter.WriteLine(bin.ToString() + "\t150\t150\t" + timer.Elapsed.TotalSeconds.ToString());
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "25", "25", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t25\t25\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
             }
 
 
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "50", "50", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t50\t50\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "60", "60", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t60\t60\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "80", "80", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t80\t80\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "100", "100", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t100\t100\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "150", "150", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t150\t150\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "200", "200", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t200\t200\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
+
+
+            for (int r = 0; r < repeats; r++)
+            {
+                timer.Restart();
+                orchestrator.Cover(new string[] { "cover", "covres.bed", "*", "500", "500", "count" });
+                timer.Stop();
+                using (var writter = new StreamWriter(UserConfig.workingDirectory + "CoverResult.txt", true))
+                {
+                    writter.WriteLine(bin.ToString() + "\t500\t500\t" + timer.Elapsed.TotalSeconds.ToString());
+                }
+            }
 
             Herald.Dispose();
         }
