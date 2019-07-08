972dd6b71c22086e245cdde99b1187bf3bd07440
VJalili <Jalili.vahid@gmail.com>
2019-07-07 17:37:07 -0700

    Refactor indexing methods and move the out of the inc folder.

---
+++ Di4/Di4/BasicOperations/IndexFunctions/BulkIndex.cs
@@ -0,0 +1,34 @@
+﻿using CSharpTest.Net.Collections;
+using Polimi.DEIB.VahidJalili.IGenomics;
+using System;
+using System.Collections.Generic;
+
+namespace Genometric.Di4
+{
+    internal class BulkIndex<C, I, M>
+        where C : IComparable<C>, IFormattable
+        where I : IInterval<C, M>
+        where M : IMetaData, new()
+    {
+        internal BulkIndex(BPlusTree<C, B> di4, SortedDictionary<C, B> bookmarks)
+        {
+            _di4 = di4;
+            _bookmarks = bookmarks;
+        }
+
+        /// <summary>
+        /// Sets and gets the _di4_1R data structure
+        /// to be manipulated. This data structure
+        /// is in common between all classes of 
+        /// namespace.
+        /// </summary>
+        private BPlusTree<C, B> _di4 { set; get; }
+
+        private SortedDictionary<C, B> _bookmarks { set; get; }
+
+        public void Index()
+        {
+
+        }
+    }
+}
