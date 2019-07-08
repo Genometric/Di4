a8ea1a5d2b054552c713c4f7882231619f8e961b
VJalili <Jalili.vahid@gmail.com>
2019-07-07 17:35:56 -0700

    Refactor higher order functions and move the out of the Inc folder.

---
+++ Di4/Di4/BasicOperations/HigherOrderFunctions/VariantOutputStrategy.cs
@@ -0,0 +1,59 @@
+﻿using Polimi.DEIB.VahidJalili.IGenomics;
+using System;
+using System.Collections.Generic;
+
+namespace Genometric.Di4
+{
+    public class VCOutputStrategy<C, I, M, O> : IOutput<C, I, M, uint>
+         where C : IComparable<C>, IFormattable
+        where I : IInterval<C, M>
+        where M : IMetaData, new()
+    {
+        public VCOutputStrategy()
+        {
+            output = new List<uint>();
+            samplesCV = new SortedDictionary<uint, int>();
+        }
+
+        public List<uint> output { set; get; }
+
+        /// <summary>
+        /// This dictionary contains IDs of samples (key) that have 
+        /// a mutation overlapping reference mutation. 
+        /// This dictionary helps to count the number of mutations
+        /// each sample has in common with the reference sample. 
+        /// The value is the count of mutations each sample has in 
+        /// common with the reference sample.
+        /// </summary>
+        public SortedDictionary<uint, int> samplesCV { set; get; }
+
+        public void Output(List<uint[]> intervals, object lockOnMe)
+        {
+            foreach (var item in intervals)
+            {
+                if (!samplesCV.ContainsKey(item[1]))
+                    samplesCV.Add(item[1], 1);
+                else
+                    samplesCV[item[1]]++;
+            }
+
+
+        }
+
+        public void Output(I interval, List<uint> intervals, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Output(C left, C right, List<uint> intervals, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Output(I interval, int count, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Di4/Di4/BasicOperations/HigherOrderFunctions/VariationAnalysis.cs
@@ -0,0 +1,87 @@
+﻿using CSharpTest.Net.Collections;
+using Polimi.DEIB.VahidJalili.IGenomics;
+using System;
+using System.Collections.Generic;
+
+namespace Genometric.Di4
+{
+    internal class VariationAnalysis<C, I, M, O>
+        where C : IComparable<C>, IFormattable
+        where I : IInterval<C, M>
+        where M : IMetaData, new()
+    {
+        internal VariationAnalysis(
+            object lockOnMe,
+            BPlusTree<C, B> di4_1R,
+            IOutput<C, I, M, O> outputStrategy,
+            List<I> intervals,
+            int start,
+            int stop,
+            C UDF,
+            C DDF,
+            SortedDictionary<uint, int> result)
+        {
+            _UDF = UDF;
+            _DDF = DDF;
+            _stop = stop;
+            _start = start;
+            _di4_1R = di4_1R;
+            _lockOnMe = lockOnMe;
+            _intervals = intervals;
+            _outputStrategy = outputStrategy;
+
+            _result = result;
+        }
+
+        private BPlusTree<C, B> _di4_1R { set; get; }
+        private int _start { set; get; }
+        private int _stop { set; get; }
+        private object _lockOnMe { set; get; }
+        private List<I> _intervals { set; get; }
+        private IOutput<C, I, M, O> _outputStrategy { set; get; }
+        private C _UDF { set; get; }
+        private C _DDF { set; get; }
+        private SortedDictionary<uint, int> _result { set; get; }
+
+        internal void Run()
+        {
+            var voutput = new VCOutputStrategy<C, I, M, uint>();
+            new Tmp__Map__for__VA<C, I, M, uint>(_lockOnMe, _di4_1R, voutput, _intervals, _start, _stop, _UDF, _DDF).Run();
+
+            _result = voutput.samplesCV;
+
+
+            
+        }
+    }
+
+
+
+    public class VOUTPUT<C, I, M> : IOutput<C, I, M, uint>
+        where C : IComparable<C>, IFormattable
+        where I : IInterval<C, M>
+        where M : IMetaData, new()
+    {
+        public List<uint> output { set; get; }
+
+        public void Output(List<uint[]> intervals, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Output(I interval, List<uint> intervals, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Output(C left, C right, List<uint> intervals, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Output(I interval, int count, object lockOnMe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
