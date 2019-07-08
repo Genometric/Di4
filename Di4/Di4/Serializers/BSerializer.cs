7f233d550a66ef93ce89ef5b5ebd9ab4297d0c1f
VJalili <Jalili.vahid@gmail.com>
2019-07-07 17:38:10 -0700

    Refactor serializers and move any one inside INC out of it.

---
+++ Di4/Di4/Serializers/BSerializer.cs
@@ -0,0 +1,31 @@
+﻿using CSharpTest.Net.Serialization;
+using System.Collections.ObjectModel;
+
+namespace Genometric.Di4
+{
+    class BSerializer : ISerializer<B>
+    {
+        private readonly ISerializer<ReadOnlyCollection<Lambda>> _lambdaArraySerializer;
+        //private readonly ISerializer<Lambda[]> _lambdaArraySerializer;
+        public BSerializer(ISerializer<ReadOnlyCollection<Lambda>> lambdaArraySerializer)
+        //    public BookmarkSerializer(ISerializer<Lambda[]> lambdaArraySerializer)
+        {
+            _lambdaArraySerializer = lambdaArraySerializer;
+        }
+
+        public B ReadFrom(System.IO.Stream stream)
+        {
+            return new B(PrimitiveSerializer.Int32.ReadFrom(stream), PrimitiveSerializer.UInt16.ReadFrom(stream), _lambdaArraySerializer.ReadFrom(stream));
+            //return new B(PrimitiveSerializer.Int32.ReadFrom(stream), PrimitiveSerializer.UInt16.ReadFrom(stream), _lambdaArraySerializer.ReadFrom(stream));
+        }
+
+        public void WriteTo(B value, System.IO.Stream stream)
+        {
+            if (value == null) return;
+
+            PrimitiveSerializer.Int32.WriteTo(value.mu, stream);
+            PrimitiveSerializer.UInt16.WriteTo(value.omega, stream);
+            _lambdaArraySerializer.WriteTo(value.lambda, stream);
+        }
+    }
+}
