7243997a0927c59243f3a5ef5f6aa6aa331b4d0e
VJalili <Jalili.vahid@gmail.com>
2019-07-28 19:24:34 -0700

    Refactor PrimaryComponents  to Model\Bookmark

---
+++ Di4/Di4/Model/Bookmark/BlockValue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Genometric.Di4
+{
+    internal class BlockValue
+    {
+        internal BlockValue(int BoundariesLowerBound, int BoundariesUpperBound, int IntervalCount, uint[] atI)
+        {
+            boundariesLowerBound = BoundariesLowerBound;
+            boundariesUpperBound = BoundariesUpperBound;            
+            intervalCount = IntervalCount;
+
+            _atI = new uint[atI.Length];
+            atI.CopyTo(_atI, 0);
+        }
+        internal BlockValue(int BoundariesLowerBound, int BoundariesUpperBound, int IntervalCount, ReadOnlyCollection<uint> atI)
+        {
+            boundariesLowerBound = BoundariesLowerBound;
+            boundariesUpperBound = BoundariesUpperBound;            
+            intervalCount = IntervalCount;
+
+            _atI = new uint[atI.Count];
+            atI.CopyTo(_atI, 0);
+        }
+
+        private uint[] _atI { set; get; }
+
+        /// <summary>
+        /// Adds inverted structure (i.e., all the intervals overlapping 
+        /// the coordinate, including the intervals overlapping with 
+        /// left-end and middle; intervals overlapping with right-end 
+        /// are not included) to the incremental snapshot 
+        /// at the start of the block.
+        /// </summary>
+        internal ReadOnlyCollection<uint> atI { get { return Array.AsReadOnly(_atI); } }
+
+        internal int boundariesUpperBound { private set; get; }
+        internal int boundariesLowerBound { private set; get; }
+        internal int intervalCount { private set; get; }
+        
+
+
+    }
+}
---
+++ Di4/Di4/Model/Bookmark/Lambda.cs
@@ -0,0 +1,46 @@
+﻿namespace Genometric.Di4
+{
+    /// <summary>
+    /// Represents the interval intersecting with 
+    /// the e of corresponding keyBookmark. 
+    /// <para>For intervals of possibly different types,
+    /// it is recommended to define this generic type
+    /// parameter in terms of Lowest Common Denominator.
+    /// </para>
+    /// </summary>
+    public struct Lambda
+    {
+        /// <summary>
+        /// Represents the interval intersecting with 
+        /// the e of corresponding keyBookmark. 
+        /// <para>For intervals of possibly different types,
+        /// it is recommended to define this generic type
+        /// parameter in terms of Lowest Common Denominator.
+        /// </para>
+        /// </summary>
+        /// <param name="phi">The intersection type of interval
+        /// wtih e of corresponding keyBookmark.</param>
+        /// <param name="atI">Descriptive hashKey of the intereval.</param>
+        internal Lambda(Phi phi, uint atI, uint collectionID)
+            : this()
+        {
+            this.phi = phi;
+            this.atI = atI;
+            this.collectionID = collectionID;
+        }
+
+
+        /// <summary>
+        /// Gets the intersection type of interval with e of corresponding keyBookmark.
+        /// </summary>
+        internal Phi phi { private set; get; }
+
+        /// <summary>
+        /// Gets hashKey of the intereval pointing to descriptive atI of 
+        /// the interval. The hashkey is represented by generic type M.
+        /// </summary>
+        internal uint atI { private set; get; }
+
+        internal uint collectionID { private set; get; }
+    }
+}
